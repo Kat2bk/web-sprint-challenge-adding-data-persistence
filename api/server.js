440524a5f6ba9b79fc12465722c5d8e07f5a77b3
unknown <Katherine2bk@gmail.com>
2021-06-27 23:20:55 -0700

    added error middleware for server.js, imported projects router, server is working perfectly so far

--- api/server.js
+++ api/server.js
@@ -1,11 +1,22 @@
 // build your server here and require it from index.js
 const express = require('express');
 const server = express()
+const projectsRouter = require('./project/router');
 // routers
 server.use(express.json())
 
+server.use('/api/projects', projectsRouter)
+
 server.get('/', (req, res) => {
     res.json({message: "Welcome to the home API, it's running!"})
+})
+
+server.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        error: {},
+        message: err.message,
+        customMessage: "Something went wrong, try again"
+    })
 })
 
 module.exports = server;
