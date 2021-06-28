0f7ee6f7c87b7c546e2f8a4f22a7d29c18bebe8c
unknown <Katherine2bk@gmail.com>
2021-06-27 23:32:55 -0700

    finished router for resources, importing into server.js

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,17 +1,19 @@
 // build your `Resource` model here
 const db = require('../../data/dbConfig');
 
-
+// get all resources
 function getAllResources() {
     return db('resources')
 }
 
+// find by id
 function getById(resource_id) {
     return db('resources')
     .where({resource_id})
     .first()
 }
 
+// create a new resource and return obj data
 function create(resource) {
     return db('resources')
     .insert(resource)
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,43 @@
 // build your `/api/resources` router here
+const express = require('express');
+const Resources = require('./model');
+const router = express.Router()
+
+// get all resources
+router.get('/', (req, res, next) => {
+    Resources.getAllResources()
+    .then(resources => {
+        res.json(resources)
+    })
+    .catch(error => {
+        next(error)
+    })
+})
+
+// find resource by id
+router.get('/:id', (req, res, next) => {
+    Resources.getById(req.params.id)
+    .then(resource => {
+        if (resource) {
+            res.json(resource)
+        } else {
+            res.status(404).json({message: "Unable to get that resource"})
+        }
+    })
+    .catch(error => {
+        next(error)
+    })
+})
+
+// create resource
+router.post('/', (req, res, next) => {
+    Resources.create(req.body)
+    .then(resource => {
+        res.status(201).json(resource)
+    })
+    .catch(error => {
+        next(error)
+    })
+})
+
+module.exports = router;
