329c3212ebee6272157e41dbb61f68c22e4ca775
unknown <Katherine2bk@gmail.com>
2021-06-27 22:40:45 -0700

    set up scripts in package.json, set up server in index.js, set up home get request in server.js, along with express

--- api/server.js
+++ api/server.js
@@ -1,1 +1,11 @@
 // build your server here and require it from index.js
+const express = require('express');
+const server = express()
+// routers
+server.use(express.json())
+
+server.get('/', (req, res) => {
+    res.json({message: "Welcome to the home API, it's running!"})
+})
+
+module.exports = server;
